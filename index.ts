105ec0fd9654ae76233f32495b98d973715ab70b
Gianni Carafa <gianni.carafa@srf.ch>
2022-05-18 22:23:01 +0200

    butify ts

--- index.ts
+++ index.ts
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port: String = process.env.PORT || "3650";
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
