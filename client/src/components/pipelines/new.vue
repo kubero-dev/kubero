a2639beb56315286393da6d3c3c97a4f3154f97e
Gianni Carafa <gianni.carafa@srf.ch>
2022-07-11 12:18:54 +0200

    gitea tab

--- client/src/components/pipelines/new.vue
+++ client/src/components/pipelines/new.vue
@@ -34,6 +34,7 @@
         >
         <v-tabs icons-and-text v-model="repotab" color="#8560A9">
             <v-tab href="#tab-github">Github <v-icon>mdi-github</v-icon> </v-tab>
+            <v-tab href="#tab-gitea">Gitea <v-icon class="gitea"></v-icon></v-tab>
             <v-tab href="#tab-gitlab">Gitlab <v-icon>mdi-gitlab</v-icon></v-tab>
             <v-tab href="#tab-bitbucket" disabled>Bitbucket <v-icon>mdi-bitbucket</v-icon></v-tab>
         </v-tabs>
@@ -85,7 +86,52 @@
           </v-tab-item>
         </v-tabs-items>
 
+        <v-tabs-items v-model="repotab">
+          <v-tab-item
+            key="1"
+            value="tab-gitea"
+          >
 
+            <v-card flat>
+              <v-card-text>
+                <v-text-field
+                  v-model="gitrepo"
+                  :rules="repositoryRules"
+                  :counter="60"
+                  label="Repository"
+                  :disabled="gitrepo_connected"
+                  required
+                ></v-text-field>
+                <v-container
+                  v-if="!gitrepo_connected"
+                >
+                  <v-btn
+                      color="primary"
+                      elevation="2"
+                      @click="connectGitea()"
+                      >Connect</v-btn>
+                </v-container>
+                <v-container
+                  v-if="gitrepo_connected"
+                >
+                  <v-alert class="alert"
+                    type="success"
+                    elevation="6"
+                    transition="scale-transition"
+                  >Webhook created
+                  </v-alert>
+                  <v-alert class="alert"
+                    type="success"
+                    elevation="6"
+                    transition="scale-transition"
+                  >Deploy keys added
+                  </v-alert>
+                </v-container>
+              </v-card-text>
+            </v-card>
+
+          </v-tab-item>
+        </v-tabs-items>
 
 
         <v-tabs-items v-model="repotab">
@@ -316,7 +362,20 @@ export default {
 .alert i.v-icon.v-icon {
   color: white !important;
 } 
-//.v-card {
-//  height: 100px;
-//}
+
+.gitea{
+    background-image: url('/img/icons/gitea.svg');
+    background-size: contain;
+    background-repeat: no-repeat;
+    filter: invert(39%) sepia(47%) saturate(584%) hue-rotate(228deg) brightness(95%) contrast(80%);
+    /*filter: invert(93%) sepia(49%) saturate(7411%) hue-rotate(184deg) brightness(87%) contrast(90%);*/
+}
+
+.gitea::before {
+    height: 23px;
+    width: 23px;
+    visibility: hidden;
+    content: "";
+}
+
 </style>
