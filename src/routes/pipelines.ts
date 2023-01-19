950d6d4df3994cc69accc63c8e47fda2e69d4fee
Gianni Carafa <gianni.carafa@srf.ch>
2023-01-19 17:57:44 +0100

    sanitize also on update

--- src/routes/pipelines.ts
+++ src/routes/pipelines.ts
@@ -184,48 +184,6 @@ Router.delete('/cli/pipelines/:pipeline/:phase/:app', bearerMiddleware, async fu
     res.send(response);
 });
 
-Router.put('/pipelines/:pipeline/:phase/:app', authMiddleware, async function (req: Request, res: Response) {
-    // #swagger.tags = ['UI']
-    // #swagger.summary = 'Update an app in a specific pipeline'
-    // #swagger.parameters['body'] = { in: 'body', description: 'App object', required: true, type: 'object' }
-
-    let appconfig: IApp = {
-        name: req.params.app,
-        pipeline: req.params.pipeline,
-        phase: req.params.phase,
-        buildpack: req.body.buildpack.name,
-        deploymentstrategy: req.body.deploymentstrategy,
-        gitrepo: req.body.gitrepo,
-        branch: req.body.branch,
-        autodeploy: req.body.autodeploy,
-        domain: req.body.domain,
-        podsize: req.body.podsize,
-        autoscale: req.body.autoscale,
-        extraVolumes: req.body.extraVolumes,
-        envVars: req.body.envvars,
-        image: {
-            containerPort: req.body.image.containerport,
-            repository: req.body.image.repository,
-            tag: req.body.image.tag || "main",
-            pullPolicy: "Always",
-            fetch: req.body.image.fetch,
-            build: req.body.image.build,
-            run: req.body.image.run,
-        },
-        web: req.body.web,
-        worker: req.body.worker,
-        cronjobs: req.body.cronjobs,
-        addons: req.body.addons,
-        resources: req.body.podsize.resources,
-    };
-
-    let app = new App(appconfig);
-
-    req.app.locals.kubero.updateApp(app, req.body.resourceVersion);
-
-    res.send("updated");
-});
-
 Router.get('/cli/pipelines/:pipeline/:phase/:app', bearerMiddleware, async function (req: Request, res: Response) {
     // #swagger.tags = ['Pipeline']
     // #swagger.summary = 'Get app details'
