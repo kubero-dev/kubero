46708abf4eda09f98d85cdcf01569301855bdff8
Gianni Carafa <gianni.carafa@srf.ch>
2023-06-26 15:32:39 +0200

    Update the regex expression to include hyphen in the service name and load a specific service from the github repository.

--- src/routes/services.ts
+++ src/routes/services.ts
@@ -23,7 +23,7 @@ Router.get('/services/:name', authMiddleware, async function (req: Request, res:
     // #swagger.tags = ['UI']
     // #swagger.summary = 'Get a specific service'
 
-    const serviceName = req.params.name.replace(/[^\w.]+/g, '');
+    const serviceName = req.params.name.replace(/[^\w.-]+/g, '');
 
     const service = await axios.get('https://raw.githubusercontent.com/kubero-dev/kubero/main/services/' + serviceName + '/app.yaml')
     .catch((err) => {
