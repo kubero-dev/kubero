9d3b03e72e18cf163bb72d1ec51bec9e0260a66d
Gianni Carafa <gianni.carafa@srf.ch>
2023-07-06 15:56:35 +0200

    change github link

--- client/src/App.vue
+++ client/src/App.vue
@@ -72,7 +72,7 @@
                         </v-list-item-icon>
                         <v-list-item-title>Documentation</v-list-item-title>
                     </v-list-item>
-                    <v-list-item link href="https://github.com/kubero-dev/kubero/discussions" target="_blank">
+                    <v-list-item link href="https://github.com/kubero-dev/kubero" target="_blank">
                         <v-list-item-icon>
                         <v-icon>mdi-github</v-icon>
                         </v-list-item-icon>
