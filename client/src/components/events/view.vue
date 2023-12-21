1a1158fff026aa46a55294ed7f0281a1cd3a7f75
Gianni Carafa <gianni.carafa@srf.ch>
2023-12-21 09:14:21 +0100

    add events placeholdder

--- client/src/components/events/view.vue
+++ client/src/components/events/view.vue
@@ -2,7 +2,8 @@
     <v-container :fluid="true">
         <h1>Events</h1>
         <v-layout>
-                <v-row>
+                <v-row
+                v-if="events.length > 0">
                     <v-timeline align-top dense>
                         <!--  v-for="n in 10" :key="n" -->
                         <v-timeline-item
@@ -21,6 +22,29 @@
                         </v-timeline-item>
 
                     </v-timeline>
+                </v-row>
+
+                <v-row
+                    v-if="events.length === 0">
+                    <v-col
+                    cols="12"
+                    md="6"
+                    style="margin-top: 20px;"
+                    >
+                        <v-alert
+                            outlined
+                            type="info"
+                            prominent
+                            border="left"
+                            style="background-color: rgba(33, 149, 243, 0.03) !important;"
+                        >
+                            <h3>No events found</h3>
+                            The default TTL for events in the Kube-API is 1 hour. If you want to 
+                            see events older events, you have to increase the TTL in the 
+                            <a href="https://kubernetes.io/docs/reference/command-line-tools-reference/kube-apiserver/" target="_blank">Kube-apiserver</a>.
+
+                        </v-alert>
+                    </v-col>
                 </v-row>
         </v-layout>
     </v-container>
