83aeaa4bdcf79106734998ad9fe369d3561d910a
Gianni Carafa <gianni.carafa@srf.ch>
2024-03-08 05:11:58 +0100

    improve forms and breadcrumbs

--- server/src/modules/config.ts
+++ server/src/modules/config.ts
@@ -126,5 +126,40 @@ export class KuberoConfig {
         for (let i = 0; i < this.buildpacks.length; i++) {
             this.buildpacks[i] = new Buildpack(kc.buildpacks[i]);
         }
+
+        for (let i = 0; i < this.podSizeList.length; i++) {
+            this.podSizeList[i] = new PodSize(kc.podSizeList[i]);
+        }
+    }
+}
+
+export class PodSize implements IPodSize {
+    public name: string;
+    public description: string;
+    public default?: boolean | undefined;
+    public resources: { 
+        requests?: { 
+            memory: string; 
+            cpu: string; 
+        } | undefined; 
+        limits?: { 
+            memory: string; 
+            cpu: string; 
+        } | undefined; 
+    };
+    constructor(ps: IPodSize) {
+        this.name = ps.name;
+        this.description = ps.description;
+        this.default = ps.default;
+        this.resources = {
+            requests: {
+                memory: ps.resources.requests?.memory || "",
+                cpu: ps.resources.requests?.cpu || ""
+            },
+            limits: {
+                memory: ps.resources.limits?.memory || "",
+                cpu: ps.resources.limits?.cpu || ""
+            }
+        }
     }
 }
