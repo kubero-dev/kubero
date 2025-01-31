e0a84d3962a4af06cef25b29d3119e4844187eae
Gianni Carafa <gianni.carafa@srf.ch>
2025-01-31 14:41:04 +0100

    Update package.json and main.ts to use port 2000 for compatibility with kubero v2.

--- server-refactored-v3/src/main.ts
+++ server-refactored-v3/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 2000); // Use port 2000 for compatibility with kubero v2
 }
 bootstrap();
