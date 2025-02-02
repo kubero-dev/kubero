6f413c19499827ea0b00002bcc16594c61cb7d00
Gianni Carafa <gianni.carafa@srf.ch>
2025-02-02 21:54:40 +0100

    add basic modules

--- server-refactored-v3/src/app.module.ts
+++ server-refactored-v3/src/app.module.ts
@@ -1,3 +1,4 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,9 +6,19 @@ import { EventsModule } from './events/events.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { AppsModule } from './apps/apps.module';
+import { PipelinesModule } from './pipelines/pipelines.module';
+import { VulnerabilitiesModule } from './vulnerabilities/vulnerabilities.module';
+import { ConfigModule } from './config/config.module';
+import { RepoModule } from './repo/repo.module';
+import { SettingsModule } from './settings/settings.module';
+import { BannerModule } from './banner/banner.module';
+import { TemplatesModule } from './templates/templates.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { LogsModule } from './logs/logs.module';
+import { DeploymentsModule } from './deployments/deployments.module';
 
 
-import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
@@ -16,6 +27,17 @@ import { join } from 'path';
     EventsModule,
     AuthModule,
     CommonModule,
+    AppsModule,
+    PipelinesModule,
+    VulnerabilitiesModule,
+    ConfigModule,
+    RepoModule,
+    SettingsModule,
+    BannerModule,
+    TemplatesModule,
+    MetricsModule,
+    LogsModule,
+    DeploymentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
