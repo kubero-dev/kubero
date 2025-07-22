f9efa708c48f61eb026707a0d96e713de69123f3
Gianni Carafa <gianni.carafa@srf.ch>
2025-07-22 08:06:43 +0200

    add portugues translations

--- client/src/plugins/i18n.ts
+++ client/src/plugins/i18n.ts
@@ -1,10 +1,10 @@
-
 import { createI18n } from 'vue-i18n'
 import en from '../locale/en'
 import ja from '../locale/ja'
 import de from '../locale/de'
 import zhHans from '../locale/zhHans'
 import deCH from '../locale/de-CH'
+import pt from '../locale/pt'
 
 // Get saved locale from localStorage or default to 'de'
 const savedLocale = localStorage.getItem('kubero.locale') || process.env.KUBERO_DEFAULT_LOCALE || 'en'
@@ -19,6 +19,7 @@ const i18n = createI18n({
     de: de,
     zhHans: zhHans,
     'de-CH': deCH,
+    pt: pt,
   },
 })
 
